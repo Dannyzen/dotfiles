fedac1010bf160381bae32d70949345aed324af2
Danny Rosen <danny.rosen@gmail.com>
2013-02-18 18:07:54 -0500

    homeshick rather than ruby homesick

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo gem install homesick
-homesick clone Dannyze/dotfiles
+curl -sL https://raw.github.com/andsens/homeshick/master/install.sh | bash
+alias homesick="$HOME/.homeshick"
+homesick clone Dannyzen/dotfiles
 homesick symlink Dannyzen/dotfiles
 vim +BundleInstall +qall
