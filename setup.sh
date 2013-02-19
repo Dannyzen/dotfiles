3a89a2fd0d386b5570b14b05dd26335e6dcee3b2
Danny Rosen <danny.rosen@gmail.com>
2013-02-19 10:39:27 -0500

    minor fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -sL https://raw.github.com/andsens/homeshick/master/install.sh | bash
 $HOME/.homeshick clone Dannyzen/dotfiles
-$HOME/.homeshick symlink Dannyzen/dotfiles
+$HOME/.homeshick symlink dotfiles
 vim +BundleInstall +qall
