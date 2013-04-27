2707e3fa2e4156664650521df9f2cb3fedb10498
Danny Rosen <danny.rosen@gmail.com>
2013-04-27 11:13:44 -0400

    linebreaks in vim an color in git diff

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@ curl -sL https://raw.github.com/andsens/homeshick/master/install.sh | bash
 $HOME/.homeshick clone Dannyzen/dotfiles
 $HOME/.homeshick symlink dotfiles
 vim +BundleInstall +qall
+
+#colorizing git configs. If i get more of these I should break this out into it's own .sh and source it here.
+git config --global color.diff always
+
