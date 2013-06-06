08f7b0a40a770606f72a44eeae32886b3c5ba2fb
Danny Rosen <danny.rosen@gmail.com>
2013-06-06 09:55:59 -0400

    homesick, not homeshick

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -sL https://raw.github.com/andsens/homeshick/master/install.sh | bash
-$HOME/.homeshick clone Dannyzen/dotfiles
-$HOME/.homeshick symlink dotfiles
+sudo gem install homesick 
+homesick clone Dannyzen/dotfiles
+homesick symlink Dannyzen/dotfiles
 vim +BundleInstall +qall
 
 #colorizing git configs. If i get more of these I should break this out into it's own .sh and source it here.
