5561391199e011baccf52c1ab88697762781892f
Danny <danny.rosen@gmail.com>
2013-06-22 19:25:13 -0400

    Verbosity is fun

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,7 +3,9 @@ emulate -L zsh
 stderr_size=$(source ~/.zshrc 2>&1 1>/dev/null )
 if [ ${#stderr_size} = 0 ]; 
     then 
+        echo "pass"
         exit 0 
     else
+        echo "fail"
         exit 1 
 fi
