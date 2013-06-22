6e4b91a43bc639aa8b54f0f8aa229f7766c150e3
Danny Rosen <danny.rosen@gmail.com>
2013-06-22 13:49:43 -0400

    adding travis fun into the mix... should work?

---
+++ .travis/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+emulate -L zsh
+setopt err_exit no_unset warn_create_global
+#setopt xtrace
+
+git ls-files -z -- '.zsh*' | while read -rd $'\0' file; do
+    [[ $file =~ \.zsh/help/.* ]] && continue
+    echo -n "Syntax checking $file... "
+    zsh -o no_exec -o no_rcs $file
+    echo "OK"
+done
