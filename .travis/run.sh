135b2fccb05372a4e8fc8a71493f51caf1a6fed8
Danny <danny.rosen@gmail.com>
2013-06-22 19:21:16 -0400

    typo fix
    stderr not sterr. durh.

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 emulate -L zsh
-sterr_size=$(source ~/.zshrc 2>&1 1>/dev/null )
-if [ ${#sterr_size} = 0 ]; 
+stderr_size=$(source ~/.zshrc 2>&1 1>/dev/null )
+if [ ${#stderr_size} = 0 ]; 
     then 
         exit 0 
     else
