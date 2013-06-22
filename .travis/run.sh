ff2a8ad5157f17bcd99a2941624afedab7ea42b4
Danny Rosen <danny.rosen@gmail.com>
2013-06-22 13:54:07 -0400

    erm

--- .travis/run.sh
+++ .travis/run.sh
@@ -4,7 +4,6 @@ setopt err_exit no_unset warn_create_global
 #setopt xtrace
 
 git ls-files -z -- '.zsh*' | while read -rd $'\0' file; do
-    [[ $file =~ \.zsh/help/.* ]] && continue
     echo -n "Syntax checking $file... "
     zsh -o no_exec -o no_rcs $file
     echo "OK"
