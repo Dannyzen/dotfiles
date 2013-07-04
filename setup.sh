a194ef8ce549890fed838a7df12b71bed0820d34
Danny <danny.rosen@gmail.com>
2013-07-04 11:43:10 -0400

    this should do it...
    ...maybe?

--- setup.sh
+++ setup.sh
@@ -3,6 +3,3 @@ sudo gem install homesick
 homesick clone Dannyzen/dotfiles
 homesick symlink Dannyzen/dotfiles
 vim +BundleInstall +qall
-
-#colorizing git configs. If i get more of these I should break this out into it's own .sh and source it here.
-git config --global color.diff always
