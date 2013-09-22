45e984a6070af553c7d39746bbef090115195dd7
Danny Rosen <danny.rosen@gmail.com>
2013-09-22 18:32:49 -0400

    note to self...

--- tools/pyradio_setup.sh
+++ tools/pyradio_setup.sh
@@ -1,2 +1,3 @@
+#PYTHON_LOCAL_SITE_PACKAGES is defined in my .zshrc
 pip install --user pyradio
 wget -q https://raw.github.com/Dannyzen/pyradio/master/pyradio/stations.csv -O $PYTHON_LOCAL_SITE_PACKAGES/pyradio/stations.csv
