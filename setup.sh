4fbefeffa6740edd09246be2429ec7c2ad61031c
Danny Rosen <danny.rosen@gmail.com>
2015-02-06 07:26:33 -0500

    bump the yakuake config symlinks

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ homesick clone git@github.com:Dannyzen/dotfiles.git
 homesick symlink 
 vim +BundleInstall +qall
 ln -s $DOTFILES/zsh-syntax-highlighting $ZSH_CUSTOM/zsh-syntax-highlighting
+ln -s $DOTFILES/kde/yakuake/yakuakerc yakuakerc
+ln -s $DOTFILES/kde/yakuake/kns_skins kns_skins
