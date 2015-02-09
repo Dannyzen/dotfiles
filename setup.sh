965a8ebf8a1d26545942f158b42ac3faf7d0e213
Danny Zen <dannyzen@users.noreply.github.com>
2015-02-09 09:00:38 -0500

    Fixing shortcuts

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ ln -s $DOTFILES/kde/yakuake/kns_skins kns_skins
 rm $HOME/.kde/share/apps/konsole/Shell.profile && ln -s $DOTFILES/kde/konsole/Shell.profile $HOME/.kde/share/apps/konsole/Shell.profile
 ln -s $DOTFILES/kde/konsole/kosoleui.rc $HOME/.kde/share/apps/konsole/konsoleui.rc
 ln -s $DOTFILES/kde/konsole/Linux.colorscheme $HOME/.kde/share/apps/konsole/Linux.colorscheme
+rm $HOME/.kde/share/config/kglobalshortcutsrc && ln -s $DOTFILES/kde/kglobalshortcutsrc $HOME/.kde/share/config/kglobalshortsrc
 rm $HOME/.kde/share/config/yakuakerc && ln -s $DOTFILES/kde/yakuake/yakuakerc $HOME/.kde/share/config/yakuakerc
 chmod -w ~/.local/share/recently-used.xbel
 chmod -w ~/.kde/share/apps/RecentDocuments/
