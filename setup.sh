fc3ad88be6c85ee43559c5fd9616550e6c6c755b
dannyzen <danny.rosen@gmail.com>
2016-02-03 11:15:24 -0500

    refactoring

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # link zsh syntax highlighting
-ln -s $DOTFILES/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh $ZSH_CUSTOM/zsh-syntax-highlighting.zsh
+ln -s $DOTFILES/zsh-syntax-highlighting/ $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+ln -s $DOTFILES/cf $ZSH_CUSTOM/plugins/cf
+ln -s $DOTFILES/
 
 # link app configs to dotfiles
 ln -s $DOTFILES/kde/share/apps/konsole $HOME/.kde/share/apps/konsole 
 ln -s $DOTFILES/kde/share/apps/yakuake $HOME/.kde/share/apps/yakuake 
-ln -s $DOTFILES/kde/share/apps/dolphin $HOME/.kde/share/apps/dolphin 
 
 
 # link rc configs to dotfiles
 ln -s $DOTFILES/kde/config/konsolerc $HOME/.kde/share/config/konsole
-ln -s $DOTFILES/kde/config/krunnerrc $HOME/.kde/share/config/krunnerrc
-ln -s $DOTFILES/kde/config/dolphinrc $HOME/.kde/share/config/dolphinrc
-ln -s $DOTFILES/kde/config/kglobalshortctsrc $HOME/.kde/share/config/kglobalshortcutsrc
+ln -s $DOTFILES/kde/config/krunnerrc $HOME/.config/krunnerrc
+ln -s $DOTFILES/kde/config/dolphinrc $HOME/.config/dolphinrc
+ln -s $DOTFILES/kde/config/kglobalshortctsrc $HOME/.config/kglobalshortcutsrc
 
 # remove write access
 chmod -w ~/.local/share/recently-used.xbel
