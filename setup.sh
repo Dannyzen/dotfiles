8bdd3b917cc92ee8c8e6bc748b9c53e12d72254b
dannyzen <danny.rosen@gmail.com>
2016-09-23 12:55:33 -0400

    adding setup for psd

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ ln -s $DOTFILES/kde/config/konsolerc $HOME/.kde/share/config/konsole
 ln -s $DOTFILES/kde/config/krunnerrc $HOME/.config/krunnerrc
 ln -s $DOTFILES/kde/config/dolphinrc $HOME/.config/dolphinrc
 ln -s $DOTFILES/kde/config/kglobalshortctsrc $HOME/.config/kglobalshortcutsrc
+ln -s $DOTFILES/home/psd ~/.config/psd
 
 # remove write access
 chmod -w ~/.local/share/recently-used.xbel
